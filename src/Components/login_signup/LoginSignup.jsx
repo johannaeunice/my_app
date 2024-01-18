4ff3427ec553495ddcd1f7959b7dfbbcd9e60bce
johannaeunice <belemejohannaeunice@gmail.com>
2024-01-18 13:48:18 +0100

    Disable line ending conversion for specific files

---
+++ src/Components/login_signup/LoginSignup.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useState } from 'react'
+import './LoginSignup.css'
+import user_icon from '../Assets/person.jfif'
+import email_icon from '../Assets/email.jfif'
+import password_icon from '../Assets/password.jfif'
+
+function Login_Signup() {
+
+  const [action, setAction] = useState("Sign Up");
+
+  return (
+    <div className='main'>
+      <div className='container'>
+        <div className="header">
+          <div className="text">{action}</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          {action==="Login"? <div></div>:<div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder='Name' />
+          </div>}
+          
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder='E-mail Id' />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder='Password' />
+          </div>
+        </div>
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password ?
+          <span>Click Here!</span></div>}
+        
+        <div className="submit-container">
+          <div className={action === "Login" ? "submit  gray" : "submit"}
+          onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+          <div className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={()=>{setAction("Login")}}>Login</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login_Signup;
