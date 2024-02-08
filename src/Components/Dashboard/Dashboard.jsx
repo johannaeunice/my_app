18e18e4ee18d758c226306f0fb2510aae7fc39e5
johannaeunice <belemejohannaeunice@gmail.com>
2024-02-08 04:44:14 +0100

    Dashboard created

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,280 +1,165 @@
-import React from 'react'
+// import React from 'react'
+import { HelmetProvider } from 'react-helmet-async'
+import './Dashboard.css'
 
 const Dashboard = () => {
-
   return (
     <div>
-        <nav className="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 shadow">
-        <h1><a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#"> Dashboard</a></h1>
-<input type="text" className="form-control form-control-primary w-100" placeholder="Search..." />
-<ul className="navbar-nav px-3">
-  <li className="nav-item text-nowrap">
-    <a className="nav-link" href="#">Logout</a>
-  </li>
-</ul>
- </nav>
- 
- <div className="container-fluid">
+      <HelmetProvider>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
+            <title>Dashboard</title>
+          </head>
+        </html>
+      </HelmetProvider>
+
+      <nav className="navbar bg-body-tertiary" data-bs-theme="dark">
+        <form className="container-fluid">
+          <div className="input-group">
+            <span className="input-group-text" id="basic-addon1">Le Nkap</span>
+            <input type="text" className="form-control" placeholder="Search..." aria-label="Username" aria-describedby="basic-addon1" />
+            <button className="btn btn-outline-success" type="submit"><i className="bi bi-search"></i></button>
+          </div>
+        </form>
+      </nav>
+
+      {/* Sidebar */}
   <div className="row">
-    {/* Sidebar */}
-    <div className="col-md-2 bg-light d-none d-md-block sidebar">
+  <div className="col-md-2 bg-light d-none d-md-block sidebar">
   <div className="left-sidebar">
   <ul className="nav flex-column sidebar-nav">
   <li className="nav-item">
     <a className="nav-link active" href="#">
-      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/></svg>
-      Candidates
+      {/* // eslint-disable-next-line react/no-unknown-property */}
+      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-Rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-Rule="evenodd"/></svg>
+      Expenses
     </a>
   </li>
   <li className="nav-item">
     <a className="nav-link" href="#">
-      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/></svg>
-      Jobs
+      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-Rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-Rule="evenodd"/></svg>
+      Income
     </a>
   </li>
   <li className="nav-item">
     <a className="nav-link" href="#">
-      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/></svg>
-      Orders
+      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-Rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-Rule="evenodd"/></svg>
+      Summary
     </a>
   </li>
   <li className="nav-item">
     <a className="nav-link" href="#">
-      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/></svg>
-      Invoices
+      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-Rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-Rule="evenodd"/></svg>
+      Savings
     </a>
   </li>
   <li className="nav-item">
     <a className="nav-link" href="#">
-      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/></svg>
-      Reports
+      <svg className="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-Rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-Rule="evenodd"/></svg>
+      Contact
     </a>
   </li>
 </ul>
   </div>
 </div>
   </div>
+
+  {/* Main */}
+  <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+  <h3>Candidates</h3>
+<hr/>
+<div className="table-responsive">
+  <table className="table table-primary">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Position</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>Project Manager</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>JS developer</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>Bird</td>
+        <td>Back-end developer</td>
+      </tr>
+      <tr>
+        <th scope="row">4</th>
+        <td>Martin</td>
+        <td>Smith</td>
+        <td>Back-end developer</td>
+      </tr>
+      <tr>
+        <th scope="row">5</th>
+        <td>Kate</td>
+        <td>Mayers</td>
+        <td>Scrum master</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
+<div className="row">
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Invoice #184382</h5>
+        <p className="card-text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+        <a href="#" className="btn btn-primary">Print</a>
+      </div>
     </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Invoice #184386</h5>
+        <p className="card-text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+        <a href="#" className="btn btn-primary">Print</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div className="row">
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Invoice #184389</h5>
+        <p className="card-text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+        <a href="#" className="btn btn-primary">Print</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Invoice #184391</h5>
+        <p className="card-text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+        <a href="#" className="btn btn-primary">Print</a>
+      </div>
+    </div>
+  </div>
+</div>
+  </main>
+</div>
+    
   )
 }
 
 export default Dashboard
-
-
-
-
-
-
-
-
-
-
-
-
-// import './Dashboard.css'
-// import dashboard_icon from '../icons/dashboard.png'
-// import expense_icon from '../icons/monitoring.png'
-// import help_icon from '../icons/contact_support.png'
-// import settings_icon from '../icons/list.png'
-// import bills_icon from '../icons/payments.png'
-// import app_icon from '../icons/logo.png'
-// import account_icon from '../icons/account_circle.png'
-// import menu_icon from '../icons/menu.png'
-// import savings_icon from '../icons/savings.png'
-// import details_icon from '../icons/expand_more.png'
-
-
-// function Dashboard() {
-//     return (
-//         <div classNameName="dashboard">
-//             <div className='top-title'>
-//                     <div className='left'>
-//                        <a href='#'>
-//                         <span>
-//                         <img src={app_icon}/> Le Nkap
-//                         </span>
-//                         </a>     
-//                     </div>
-                    
-//                     <div className='right'>
-//                         <a href='#'>
-//                             <span>
-//                                 Need our help?
-//                             </span>
-//                         </a>
-//                         <a href='#'>
-//                             <span>
-//                                 Read Our Blog
-//                             </span>
-//                         </a>
-//                         <div className='account'>
-//                             <img src={account_icon}/>
-//                             <span>
-//                                 Hi Johanna
-//                             </span>
-//                         </div>
-//                         <span>
-//                             <img src={menu_icon}/>
-//                         </span>
-//                     </div>
-//                 </div>
-//             <div className="container">
-                
-//                 <div className='aside-left'>
-//                     <div className='top'>
-//                         <a href='#'>
-//                     <span>
-//                             <img src={dashboard_icon} alt="dashboard"/> Dashboard
-//                         </span>
-//                         </a>
-//                         <a href='#'>
-//                         <span>
-//                             <img src={bills_icon} alt='Bills & Payments'/> Bills and Payments
-//                         </span>
-//                         </a>
-//                         <a href='#'>
-//                         <span>
-//                             <img src={expense_icon} alt='Expenses'/> Expenses
-//                             <span className='now'>Now</span>
-//                         </span>
-//                         </a>
-//                         <a href='#'>
-//                         <span>
-//                             <img src={settings_icon} alt='Settings'/>Settings
-//                         </span>
-//                         </a>
-//                         <a href='#'>
-//                         <span>
-//                             <img src={help_icon} alt='Get Help'/> Get Help
-//                         </span>
-//                         </a>
-//                     </div>
-//                     <div className='bottom'>
-//                     <span>
-//                                 {/* add img for all rights reserved */}
-//                                 All Rights Reserved 2023-2024
-//                             </span>
-//                     </div>
-//                 </div>
-
-//                 <main>
-//                     <div className='top'>
-//                         <div className='box'>
-//                             <div className="dot">
-//                             </div>
-//                             <span>Food & Drinks</span>
-//                             <div className='amount'>
-//                                 {/* <img src={roupi_icon} */}
-//                                 18,046
-//                             </div>
-//                         </div>
-//                         <div className='box'>
-//                             <div className="dot">
-//                             </div>
-//                             <span>Bills & Payments</span>
-//                             <div className='amount'>
-//                                 {/* <img src={roupi_icon} */}
-//                                 46,24
-//                             </div>
-//                         </div>
-//                         <div className='box'>
-//                             <div className="dot">
-//                             </div>
-//                             <span>Entertainment</span>
-//                             <div className='amount'>
-//                                 {/* <img src={roupi_icon} */}
-//                                 2376
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div className='bottom'>
-//                         <div className='chart-box'>
-
-//                         </div>
-//                     </div>
-//                 </main>
-
-//                 <aside className='right'>
-//                     <div className='header'>
-//                         <span>
-//                             <img src={savings_icon}/>
-//                             Your transaction History
-//                         </span>
-//                     </div>
-//                     <span className='line'></span>
-//                     <div className='data'>
-//                         <div className='item'>
-//                             <button>
-//                                 <img src={bills_icon}/>
-//                             </button>
-//                             <span className='title'>Burger King</span>
-//                             <span className='date'>11th Dec 2017, Monday</span>
-//                             <div className='amount'>
-//                                 <span className='red'>
-//                                 {/* <img src={roupi_icon} */} 3,197
-//                                 </span>
-//                             </div>
-//                             <img src={details_icon}/>
-//                         </div>
-//                         <span className='line'></span>
-//                         <div className='item'>
-//                             <button>
-//                                 <img src={bills_icon}/>
-//                             </button>
-//                             <span className='title'>Arun Payment</span>
-//                             <span className='date'>11th Dec 2017, Monday</span>
-//                             <div className='amount'>
-//                                 <span className='green'>
-//                                 {/* <img src={roupi_icon} */} 20,000
-//                                 </span>
-//                             </div>
-//                             <img src={details_icon}/>
-//                         </div>
-//                         <span className='line'></span>
-//                         <div className='item'>
-//                             <button>
-//                                 <img src={bills_icon}/>
-//                             </button>
-//                             <span className='title'>PVR Forum</span>
-//                             <span className='date'>11th Dec 2017, Monday</span>
-//                             <div className='amount'>
-//                                 <span className='red'>
-//                                 {/* <img src={roupi_icon} */} 2,197
-//                                 </span>
-//                             </div>
-//                             <img src={details_icon}/>
-//                         </div>
-//                         <span className='line'></span>
-//                         <div className='item'>
-//                             <button>
-//                                 <img src={bills_icon}/>
-//                             </button>
-//                             <span className='title'>Mc Donald s, HSR</span>
-//                             <span className='date'>11th Dec 2017, Monday</span>
-//                             <div className='amount'>
-//                                 <span>
-//                                 {/* <img src={roupi_icon} */} 3,103
-//                                 </span>
-//                             </div>
-//                             <img src={details_icon}/>
-//                         </div>
-//                         <span className='line'></span>
-//                     </div>
-//                     <div className='bottom'>
-//                         <span className='line'></span>
-//                         <img src={expense_icon}/>
-//                         <span>Missing Transaction ?</span>
-//                         <div className='btn'>
-//                             <button>
-//                                 ADD NEW
-//                             </button>
-//                         </div>
-//                     </div>
-//                 </aside>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Dashboard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-// import Dashboard from './Components/Dashboard/Dashboard';
+import Dashboard from './Components/Dashboard/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App';
+// import App from './App';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Router>   */}
-     <App/>
-     {/* <Dashboard/> */}
+     {/* <App/> */}
+     <Dashboard/>
     {/* </Router> */}
     {/* <RouterProvider router={router}/> */}
     
