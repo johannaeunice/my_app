5d2c2502fcfc9da78f40fa45105e86ce4e6d22f4
johannaeunice <belemejohannaeunice@gmail.com>
2025-02-20 13:22:07 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import Dashboard from './Components/Dashboard/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import App from './App';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Router>   */}
-     {/* <App/> */}
-     <Dashboard/>
-    {/* </Router> */}
-    {/* <RouterProvider router={router}/> */}
     
   </React.StrictMode>,
 )
